e5f87ea936dcd387ff0cf071065efb9e78ead4c4
mikeythacker <mikeythacker@icloud.com>
2024-10-24 16:23:45 +0100

    Arrays

--- Arrays.c
+++ Arrays.c
@@ -1,1 +1,20 @@
-void arrays() {}
+void arrays() {
+    /*
+     Arrays are just allocated contiguous blocks of memory
+     They are objects, you cannot access properties like length
+     Maximum length of the array must be explicitly recorded. Cannot be changed
+
+     e.g., int a[10]; allocates space for 10 integers in one continuous block
+
+
+    The name of the array variable can be used as a poniter
+    In int *pointer = a, we know that "a" is a pointer to the first element in the array
+
+    Array Itteration
+    ----------------
+    int i;
+    for (i = 0; i < 10; i++) {
+        // i = index, a = array
+        printf("Value at %d is %d\n", i, a[i]
+     */
+}

9a8ab2087f9e54b6f47a17e2177833ee53d31bc5
mikeythacker <mikeythacker@icloud.com>
2024-10-24 15:55:40 +0100

    Data types, arithmetic, and pointers

---
+++ cmake-build-debug/CMakeFiles/CLion_Projects.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mikeythacker/Desktop/Uni/Learing C/ConditionalStatements.c" "CMakeFiles/CLion_Projects.dir/ConditionalStatements.c.o" "gcc" "CMakeFiles/CLion_Projects.dir/ConditionalStatements.c.o.d"
+  "/Users/mikeythacker/Desktop/Uni/Learing C/DataTypes.c" "CMakeFiles/CLion_Projects.dir/DataTypes.c.o" "gcc" "CMakeFiles/CLion_Projects.dir/DataTypes.c.o.d"
+  "/Users/mikeythacker/Desktop/Uni/Learing C/HelloWorld.c" "CMakeFiles/CLion_Projects.dir/HelloWorld.c.o" "gcc" "CMakeFiles/CLion_Projects.dir/HelloWorld.c.o.d"
+  "/Users/mikeythacker/Desktop/Uni/Learing C/Pointers.c" "CMakeFiles/CLion_Projects.dir/Pointers.c.o" "gcc" "CMakeFiles/CLion_Projects.dir/Pointers.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CLion_Projects.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CLion_Projects"
+  "CLion_Projects.pdb"
+  "CMakeFiles/CLion_Projects.dir/ConditionalStatements.c.o"
+  "CMakeFiles/CLion_Projects.dir/ConditionalStatements.c.o.d"
+  "CMakeFiles/CLion_Projects.dir/DataTypes.c.o"
+  "CMakeFiles/CLion_Projects.dir/DataTypes.c.o.d"
+  "CMakeFiles/CLion_Projects.dir/HelloWorld.c.o"
+  "CMakeFiles/CLion_Projects.dir/HelloWorld.c.o.d"
+  "CMakeFiles/CLion_Projects.dir/Pointers.c.o"
+  "CMakeFiles/CLion_Projects.dir/Pointers.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLion_Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
